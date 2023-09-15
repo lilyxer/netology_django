d1d32aee0c8daad8c6afd4f4ad4873074832f938
Mikhail Borisov <borisov.uk@gmail.com>
2023-09-15 22:11:52 +0300

    added a solution to the ыеуз 1.1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,35 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
-
+from datetime import datetime, timedelta
+from os import listdir, getcwd
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
-    pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
-    }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
-    context = {
-        'pages': pages
-    }
+    pages = {'Главная страница': reverse('home'),
+             'Показать текущее время': reverse('time'),
+             'Показать содержимое рабочей директории': reverse('workdir'),}
+    context = {'pages': pages}
     return render(request, template_name, context)
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    template_name = 'app/time.html'
+    time_now = datetime.utcnow() + timedelta(hours=3)
+    time_now = time_now.strftime("%M:%H:%S")
+    context = {'time': {'key': 'Текущее время: ',
+                        'value': time_now},
+               'page': {'key': 'Возврат на главную страницу', 
+                        'value': reverse('home')},}
+    return render(request, template_name, context)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    template_name = 'app/workdir.html'
+    context = {'directory': {'key': 'Ваша директория: ', 
+                             'value': f'{getcwd()}'},
+               'file_list': {'key': 'Список файлов: ', 
+                             'value': listdir()}, 
+               'page': {'key': 'Возврат на главную страницу', 
+                        'value': reverse('home')},}
+    return render(request, template_name, context)
+
